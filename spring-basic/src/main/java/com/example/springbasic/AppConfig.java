bdb3a1ea137a174012a4df1c232ebf625194afe4
MWJOB <okkilltogo@gmail.com>
2025-03-13 17:22:24 +0900

    새로운 할인 정책 적용

--- spring-basic/src/main/java/com/example/springbasic/AppConfig.java
+++ spring-basic/src/main/java/com/example/springbasic/AppConfig.java
@@ -2,7 +2,10 @@ package com.example.springbasic;
 
 import com.example.springbasic.Order.OrderService;
 import com.example.springbasic.Order.OrderServiceImpl;
+import com.example.springbasic.discount.DiscountPolicy;
 import com.example.springbasic.discount.FixDiscountPolicy;
+import com.example.springbasic.discount.RateDiscountPolicy;
+import com.example.springbasic.member.MemberRepository;
 import com.example.springbasic.member.MemberService;
 import com.example.springbasic.member.MemberServiceImpl;
 import com.example.springbasic.member.MemoryMemberRepository;
@@ -10,10 +13,20 @@ import com.example.springbasic.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService(){
-     return new MemberServiceImpl(new MemoryMemberRepository());
+     return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //역활을 확실히 볼 수 있도록 Extract Method 해주는것이 좋다.
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
