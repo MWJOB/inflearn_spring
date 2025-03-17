946814284d73c1a11e25f6a13002d37c1038ae80
MWJOB <okkilltogo@gmail.com>
2025-03-18 01:53:56 +0900

    필터

---
+++ spring-basic/src/test/java/com/example/springbasic/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,14 @@
+package com.example.springbasic.scan.filter;
+
+
+import java.lang.annotation.*;
+
+
+//얘가 붙은 것은 컴포넌트 스캔에 제외할 거라는 어노테이션
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) //타입은 클래스 레벨에 붙는다는것
+@Documented
+public @interface MyExcludeComponent {
+
+
+}
