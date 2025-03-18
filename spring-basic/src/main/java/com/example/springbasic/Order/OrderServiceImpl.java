76c122499ae946b7495216e284a41b5d97e07b82
MWJOB <okkilltogo@gmail.com>
2025-03-18 18:34:41 +0900

    Lombok, @Qualifier

--- spring-basic/src/main/java/com/example/springbasic/Order/OrderServiceImpl.java
+++ spring-basic/src/main/java/com/example/springbasic/Order/OrderServiceImpl.java
@@ -1,25 +1,30 @@
 package com.example.springbasic.Order;
 
+import com.example.springbasic.annotation.MainDiscountPolicy;
 import com.example.springbasic.discount.DiscountPolicy;
 import com.example.springbasic.member.Member;
 import com.example.springbasic.member.MemberRepository;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    //의존 관계를 추가하고 싶으면 여기에다 private final ex ex 로 추가해주면 의존관계가 자동으로 추가됨.
 
+    //@RequiredArgsConstructor 가 만들어줌
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- spring-basic/src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
+++ spring-basic/src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
@@ -1,10 +1,12 @@
 package com.example.springbasic.discount;
 
+import com.example.springbasic.annotation.MainDiscountPolicy;
 import com.example.springbasic.member.Grade;
 import com.example.springbasic.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int discountPercent = 10;
